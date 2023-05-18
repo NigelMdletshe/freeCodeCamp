08b9b60ceaeab2618045e8174383e01d190fb656
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-18 12:12:32 +0200

    strings in js

--- Basic Java Script/script.js
+++ Basic Java Script/script.js
@@ -13,3 +13,21 @@ myNum = myVar;
 // Declare string Variables
 var myName;
 var myName = "your name";
+
+//Explore Differences Between the var and let Keywords
+var camper = "James";
+var camper = "David";
+console.log(camper);
+
+// One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations
+
+/*
+onst has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
+
+*/ 
+const FAV_PET = "Cats";
+
+//Escaping Literal Quotes in Strings
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+const doubleQuoteStr = "This is a string"; 
+const singleQuoteStr = 'This is also a string';
