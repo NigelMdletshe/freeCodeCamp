2c03ca19ed54e409da9f5085516c4644f984b2e6
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-19 23:51:35 +0200

    == operator

--- Basic Java Script/script.js
+++ Basic Java Script/script.js
@@ -204,3 +204,20 @@ function test (myCondition) {
 
 test(true);
 test(false);
+
+//Comparison with the Equality Operator
+
+/* 
+There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.
+
+The most basic operator is the equality operator ==. 
+The equality operator compares two values and returns true if they're equivalent or false if they are not.
+ Note that equality is different from assignment (=), which assigns the value on the right of the operator to a variable on the left.
+
+*/
+function equalityTest(myVal) {
+  if (myVal == 10) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
