ae48c43d851808a6e56357c5c84d19c59cb0bbe1
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-23 23:16:05 +0200

    else if

--- Basic Java Script/script.js
+++ Basic Java Script/script.js
@@ -342,3 +342,22 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+//Introducing Else Statements
+/* When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.*/
+
+if (num > 10) {
+  return "Bigger than 10";
+} else {
+  return "10 or Less";
+}
+
+//Introducing Else If Statements
+/* If you have multiple conditions that need to be addressed, you can chain if statements together with else if statements.*/
+if (num > 15) {
+  return "Bigger than 15";
+} else if (num < 5) {
+  return "Smaller than 5";
+} else {
+  return "Between 5 and 15";
+}
