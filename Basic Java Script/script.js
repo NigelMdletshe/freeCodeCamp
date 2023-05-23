0f3ee0cbdd19432a0ff0d7dd6f71d0193dcd67f7
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-23 23:56:07 +0200

    golf game code

--- Basic Java Script/script.js
+++ Basic Java Script/script.js
@@ -427,4 +427,26 @@ par + 2	"Double Bogey"
 par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
 */
 
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if(strokes == 1){
+    return "Hole-in-one!";
+  }else if(strokes<= par - 2 ){
+    return "Eagle";
+  }else if(strokes == par - 1){
+    return "Birdie";
+  }else if(strokes == par){
+    return "Par";
+  }else if(strokes == par + 1){
+    return "Bogey";
+  }else if(strokes == par + 2){
+    return "Double Bogey";
+  } else{
+
+  return "Change Me";}
+  // Only change code above this line
+}
+
+golfScore(5, 4);
