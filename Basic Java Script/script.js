5c184ade2f70a138061cedd07c0a8bb019a82d6a
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-23 22:35:41 +0200

    Comparison with the Strict Inequality Operator

--- Basic Java Script/script.js
+++ Basic Java Script/script.js
@@ -257,3 +257,22 @@ Examples */
 1 != '1'   // false
 1 != true  // false
 0 != false // false
+
+/* Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99.*/
+// Setup
+function testNotEqual(val) {
+  if (val!= 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+//Comparison with the Strict Inequality Operator
+/*The strict inequality operator (!==) is the logical opposite of the strict equality operator. It means "Strictly Not Equal" and returns false where strict equality would return true and vice versa. The strict inequality operator will not convert data types.
+
+Examples */
+3 !==  3  // false
+3 !== '3' // true
+4 !==  3  // true
