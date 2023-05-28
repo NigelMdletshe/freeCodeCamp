8437aa2970b7694d42e2497126bd54a06b73c776
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-28 23:56:21 +0200

    Return Early Pattern for Functions

--- Basic Java Script/script.js
+++ Basic Java Script/script.js
@@ -571,3 +571,16 @@ function isEqual(a, b) {
 function isEqual(a, b) {
   return a === b;
 }
+
+//Return Early Pattern for Functions
+/* When a return statement is reached, the execution of the current function stops 
+and control returns to the calling location.*/
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+/* The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console,
+ because the function exits at the return statement */
